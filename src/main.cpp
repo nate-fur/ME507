cdc19d66b32174b5d67318f89d0b8dcdf25da4e4
Nathaniel Furbeyre <nmfurbearr@outlook.com>
2018-11-08 10:47:57 -0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,14 @@
 //
 
 #include <iostream>
+#include "Adafruit_BNO055.h"
 using namespace std;
 
 
 int main() {
+    Adafruit_BNO055 imu = new Adafruit_BNO055();
+
+
 
 
     cout << "Hello world" << endl;
